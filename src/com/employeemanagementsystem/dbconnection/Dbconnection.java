3783862e84d285e145661284479c9032a459ce0e
Avijit <avisrabon22@gmail.com>
2021-10-20 04:46:15 +0530

    database connection update

--- src/com/employeemanagementsystem/dbconnection/Dbconnection.java
+++ src/com/employeemanagementsystem/dbconnection/Dbconnection.java
@@ -3,14 +3,22 @@ package com.employeemanagementsystem.dbconnection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class Dbconnection {
 
+public class Dbconnection {
 	public static Connection getconnect() {
+		
+		
+		String DB_URL=System.getenv("DB_URL");
+		String DB_Port=System.getenv("DB_Port");
+		String DB_Name=System.getenv("DB_Name");
+		String DB_Username=System.getenv("DB_Username");
+		String DB_pass=System.getenv("DB_pass");
 		Connection con = null;
 		try {
+			
 			if(con==null) {
 				Class.forName("com.mysql.cj.jdbc.Driver");
-				con=DriverManager.getConnection("jdbc:mysql://localhost:8000/employeemanagementsystem","avijit","Avijit");
+				con=DriverManager.getConnection("jdbc:mysql://"+DB_URL+":3306/"+DB_Name+","+DB_Username+","+DB_pass);
 //				System.out.println("Connected with Database");
 			}
 			
