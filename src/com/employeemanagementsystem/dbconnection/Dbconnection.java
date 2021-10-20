0774b6b476c54af0bf2e47552c9d36aae0b7bf69
Avijit <avisrabon22@gmail.com>
2021-10-21 04:31:28 +0530

    db connection update

--- src/com/employeemanagementsystem/dbconnection/Dbconnection.java
+++ src/com/employeemanagementsystem/dbconnection/Dbconnection.java
@@ -3,22 +3,14 @@ package com.employeemanagementsystem.dbconnection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-
 public class Dbconnection {
+
 	public static Connection getconnect() {
-		
-		
-		String DB_URL=System.getenv("DB_URL");
-		String DB_Port=System.getenv("DB_Port");
-		String DB_Name=System.getenv("DB_Name");
-		String DB_Username=System.getenv("DB_Username");
-		String DB_pass=System.getenv("DB_pass");
 		Connection con = null;
 		try {
-			
 			if(con==null) {
 				Class.forName("com.mysql.cj.jdbc.Driver");
-				con=DriverManager.getConnection("jdbc:mysql://"+DB_URL+":3306/"+DB_Name+","+DB_Username+","+DB_pass);
+				con=DriverManager.getConnection("jdbc:mysql://ls-dba2334cde9acd836a7f3ed5c2e27834bcd09eb1.crgfoczgg7zq.ap-south-1.rds.amazonaws.com:3306/employeemanagementsystem","dbmasteruser","Avijit3834");
 //				System.out.println("Connected with Database");
 			}
 			
