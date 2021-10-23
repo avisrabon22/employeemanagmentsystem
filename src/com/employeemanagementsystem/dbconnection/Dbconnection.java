2950b5c49a4bffc6af2a30ca6fcba5d04fcc4218
avisrabon22@gmail.com <avisrabon22@gmail.com>
2021-10-24 02:20:20 +0530

    dbconnection update

--- src/com/employeemanagementsystem/dbconnection/Dbconnection.java
+++ src/com/employeemanagementsystem/dbconnection/Dbconnection.java
@@ -10,13 +10,11 @@ public class Dbconnection {
 		try {
 			if (con == null) {
 				Class.forName("com.mysql.cj.jdbc.Driver");
-<<<<<<< HEAD
-				con=DriverManager.getConnection("jdbc:mysql://localhost:8000/employeemanagementsystem","avijit","Avijit");
-=======
+
 				con = DriverManager.getConnection(
 						"jdbc:mysql://ls-dba2334cde9acd836a7f3ed5c2e27834bcd09eb1.crgfoczgg7zq.ap-south-1.rds.amazonaws.com:3306/employeemanagementsystem",
 						"dbmasteruser", "Avijit3834");
->>>>>>> f25f2477bb76b262ef24476bc6c11b90f3c840df
+
 //				System.out.println("Connected with Database");
 			}
 
