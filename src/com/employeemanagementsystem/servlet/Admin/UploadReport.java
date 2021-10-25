4a90eccfc2e4f706592135e95ef6fcfc9d489e5f
Avijit <avisrabon22@gmail.com>
2021-10-26 04:51:26 +0530

    updated main module

---
+++ src/com/employeemanagementsystem/servlet/Admin/UploadReport.java
@@ -0,0 +1,29 @@
+package com.employeemanagementsystem.servlet.Admin;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/UploadReport")
+public class UploadReport extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public UploadReport() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
